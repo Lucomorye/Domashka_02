15df922facf44590af25f29ce4ccc70b62dfdd7d
Tarantin <mr.fadeyka@gmail.com>
2023-07-11 09:17:53 +0300

    added task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+Console.WriteLine("Введите трёх значное число");
+int number = int.Parse(Console.ReadLine());
+if((number > 99 && number < -99) || (number < 1000 && number > -1000))
+{
+  number = number / 10 % 10;
+  Console.WriteLine(number);
+}
+else
+{
+  Console.WriteLine("Вы ввели не трёхзначное число");
+}
